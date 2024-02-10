69e1c5b36348f15bf819b95a4952dda153ecaf26
shefattonmoy <shefathossain7@gmail.com>
2024-02-10 14:16:03 +0600

    second commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,67 @@
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
+    <footer>
+      <Container>
+        <Row>
+          <Col lg="4" md="6" sm="12" className="mb-4">
+            <div>
+              <div>
+                <h4>BizConnect</h4>
+              </div>
+              <p className="mt-4 text-black">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Blanditiis, nobis.
+              </p>
+            </div>
+          </Col>
+          <Col lg="4" md="6" sm="12" className="mb-4">
+            <div>
+              <h4>Our Services</h4>
+              <ListGroup className="mb-3">
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Basic</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Intermediate</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Pro</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="4" md="6" sm="12" className="mb-4">
+            <div className="quick-links">
+              <h4 className="quick-links-title">Contact Us</h4>
+              <ListGroup className="mb-3">
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span><i className="ri-map-pin-line"></i></span>
+                  <p className="text-black">Dhaka, Bangladesh</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                <span><i className="ri-phone-line"></i></span>
+                  <p className="text-black">+880123456789</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                <span><i className="ri-mail-line"></i></span>
+                  <p className="text-black">contact@bizconnect.bd</p> 
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="12">
+            <p className="mt-4 text-center text-black">
+              Copyright {year} Developed by BizConnect Inc. All Rights Reserved.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Container, Row } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import userIcon from "../../assets/images/user-icon.png";
 import { motion } from "framer-motion";
+import { useRef, useEffect } from "react";
 
 const navLink = [
   {
@@ -20,8 +21,34 @@ const navLink = [
 ];
 
 const Header = () => {
+
+  const headerRef = useRef(null);
+
+  const menuRef = useRef(null);
+
+  const stickyHeaderFunction = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky-header");
+      } else {
+        headerRef.current.classList.remove("sticky-header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    stickyHeaderFunction();
+
+    return () => window.removeEventListener("scroll", stickyHeaderFunction);
+  });
+
+  const menuToggle = () => menuRef.current.classList.toggle("active-menu");
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <Container>
         <Row>
           <div className="nav-wrapper">
@@ -29,7 +56,7 @@ const Header = () => {
               <h1>BizConnect</h1>
             </div>
 
-            <div className="navigation">
+            <div className="navigation" ref={menuRef} onClick={menuToggle}>
               <ul className="menu">
                 {navLink.map((item, index) => (
                   <li className="nav-item" key={index}>
@@ -50,12 +77,11 @@ const Header = () => {
               <span>
                 <motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt="" />
               </span>
-            </div>
-
-            <div className="mobile-menu">
-              <span>
-                <i className="ri-menu-line"></i>
-              </span>
+              <div className="mobile-menu">
+                <span onClick={menuToggle}>
+                  <i className="ri-menu-line"></i>
+                </span>
+              </div>
             </div>
           </div>
         </Row>
